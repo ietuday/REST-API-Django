95157c11c3561a5246ca549aa74e56f386f019ed
Udayaditya Singh <udayaditya.singh@gmail.com>
2020-02-23 01:16:39 +0530

    changes

--- profille-rest-api/profiles_api/models.py
+++ profille-rest-api/profiles_api/models.py
@@ -3,29 +3,42 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
+# Create your models here.
+
 
 class UserProfileManager(BaseUserManager):
-    """Manager for user profile"""
+    """Helps Django work with our custom user model."""
+
     def create_user(self, email, name, password=None):
-        """Create a new User Profile"""
+        """Creates a new user profile object."""
+
         if not email:
-            raise ValueError("User must have an email address")
+            raise ValueError('Users must have an email address.')
+
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
+
         user.set_password(password)
         user.save(using=self._db)
 
-    def create_super_user(self, email, name, password):
-        """Create and save new super user with given details"""
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details."""
+
         user = self.create_user(email, name, password)
+
         user.is_superuser = True
         user.is_staff = True
+
         user.save(using=self._db)
+
         return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Database model for users in the system"""
+    """Respents a "user profile" inside our system."""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -37,15 +50,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrieve Full name of the user"""
+        """Used to get a users full name."""
+
         return self.name
 
-    def get_sort_name(self):
-        """Retrieve sort name of the user"""
+    def get_short_name(self):
+        """Used to get a users short name."""
+
         return self.name
 
     def __str__(self):
-        """Return String representaion of our user"""
+        """Django uses this when it needs to convert the object to a string"""
+
         return self.email
-
-
--- profille-rest-api/profiles_api/views.py
+++ profille-rest-api/profiles_api/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from . import serializers
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django view',
+            'Gives you the most control over you application logic',
+            'Is mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
