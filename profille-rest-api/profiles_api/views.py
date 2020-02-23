36d018019b0b14292481fd8916a8597b2a6bfc75
Udayaditya Singh <udayaditya.singh@gmail.com>
2020-02-23 10:16:39 +0530

    changes

--- profille-rest-api/profiles_api/views.py
+++ profille-rest-api/profiles_api/views.py
@@ -5,16 +5,20 @@ from rest_framework import status
 from . import serializers
 
 
+# Create your views here.
+
 class HelloApiView(APIView):
-    """Test API View"""
+    """Test API View."""
+
     serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
-        """Returns a list of APIView features"""
+        """Returns a list of APIView features."""
+
         an_apiview = [
             'Uses HTTP methods as function (get, post, patch, put, delete)',
-            'Is similar to a traditional Django view',
-            'Gives you the most control over you application logic',
+            'It is similar to a traditional Django view',
+            'Gives you the most control over your logic',
             'Is mapped manually to URLs'
         ]
 
@@ -22,7 +26,8 @@ class HelloApiView(APIView):
 
     def post(self, request):
         """Create a hello message with our name."""
-        serializer = self.serializer_class(data=request.data)
+
+        serializer = serializers.HelloSerializer(data=request.data)
 
         if serializer.is_valid():
             name = serializer.data.get('name')
@@ -31,3 +36,23 @@ class HelloApiView(APIView):
         else:
             return Response(
                 serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        """Patch request, only updates fields provided in the request."""
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """Deletes and object."""
+
+        return Response({'method': 'delete'})
+
+
+
+
+
