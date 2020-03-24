ba001e47fe921bec5f5342701936cf87b6f9d4e0
Udayaditya Singh <udayaditya.singh@gmail.com>
2020-03-24 11:52:50 +0530

    changes

--- profile-rest-api-2/profiles_api/models.py
+++ profile-rest-api-2/profiles_api/models.py
@@ -5,54 +5,59 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
-    """Manager for user profile"""
+    """Helps Django work with our custom user model."""
 
     def create_user(self, email, name, password=None):
-        """Create a new User Profile"""
+        """Creates a new user profile object."""
+
         if not email:
-            raise ValueError("User must have an email address")
+            raise ValueError('Users must have an email address.')
+
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
         user.set_password(password)
-        user.save(using= self._db)
+        user.save(using=self._db)
 
         return user
 
-
     def create_superuser(self, email, name, password):
-        """Create and save a new super user with given details"""
+        """Creates and saves a new superuser with given details."""
+
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
-        user.save(using = self._db)
+
+        user.save(using=self._db)
 
         return user
 
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Database model for users in the system"""
-    email = models.EmailField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
+    """Respents a "user profile" inside our system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrieve full name of user"""
+        """Used to get a users full name."""
+
         return self.name
 
-    def get__short__name(self):
-        """Retrieve short name of user"""
-        return  self.name
+    def get_short_name(self):
+        """Used to get a users short name."""
+
+        return self.name
 
     def __str__(self):
-        """Return String representation of our user"""
-        return self.email
+        """Django uses this when it needs to convert the object to a string"""
 
-    
-    
+        return self.email
